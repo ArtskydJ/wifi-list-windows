5f8266e9f6e18a0dba017b3ebbe57732e334962f
Joseph Dykstra <josephdykstra@gmail.com>
2016-11-25 11:21:20 -0600

    mode=bssid, stupid

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var cp = require('child_process')
 var parse = require('./parse.js')
 
 module.exports = function list(cb) {
-	cp.exec('netsh wlan show networks', function (err, networksText) {
+	cp.exec('netsh wlan show networks mode=bssid', function (err, networksText) {
 		if (err) return cb(err)
 
 		cb(null, parse(networksText))
